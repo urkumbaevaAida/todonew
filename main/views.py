f65ab0284478d593bedbbc8c5564ec0f2953504b
urkumbaevaAida <ai88@mail.ru>
2021-01-29 02:59:17 +0600

    added todo closing

--- main/views.py
+++ main/views.py
@@ -33,4 +33,10 @@ def mark_todo(request, id):
     todo.save()
     return redirect(test)
 
+def close_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_closed = not todo.is_closed
+    todo.save()
+    return redirect(test)
+
     
