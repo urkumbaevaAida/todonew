2e991600701401f341d5381a3f2cff6f110ae0f1
urkumbaevaAida <ai88@mail.ru>
2021-01-29 02:40:38 +0600

    added todo form

---
+++ main/migrations/0002_auto_20210114_2047.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='todo',
+            name='created_at',
+            field=models.DateTimeField(auto_now_add=True),
+        ),
+    ]
